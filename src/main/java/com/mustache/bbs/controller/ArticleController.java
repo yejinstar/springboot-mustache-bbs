4ae4f6f22f19c9eaa2c92dfa0ff3597079d1394d
yejinstar <gyj5586@gmail.com>
2022-11-09 11:40:38 +0900

    ArticleController list, {id} 작성

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -5,9 +5,14 @@ import com.mustache.bbs.domain.entity.Article;
 import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -24,13 +29,37 @@ public class ArticleController {
         return "articles/new";
     }
 
-    @PostMapping(value = "/posts")
+    @PostMapping(value = "")
     public String createArticle(ArticleDto articleDto) {
         // 실무에서 println 안씀 로그를 쓴다(서버에서 일어나는 일을 기록하는것)
-        log.info(articleDto.toString());
-        Article article = articleDto.toEntity();
-        articleRepository.save(article);
-        return "";
+        log.info(articleDto.getTitle());
+        log.info(articleDto.getContent());
+        Article savedArticle = articleRepository.save(articleDto.toEntity());
+        log.info("generatedID:{}",savedArticle.getId());
+        return String.format("redirect:/articles/%d", savedArticle.getId());
+    }
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "articles/list";
     }
 
+    @GetMapping("")
+    public String index() {
+        return "redirect:/articles/list";
+    }
+
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if (!optArticle.isEmpty()) {
+            model.addAttribute("article", optArticle.get());
+            return "articles/show";
+        } else{
+            return "articles/error";
+        }
+
+    }
 }
