b753c5ea829d421985da30e4c6967feb42e8ebc6
yejinstar <gyj5586@gmail.com>
2022-11-11 13:51:39 +0900

    페이징 기능 service 단위 ...?

---
+++ src/main/java/com/mustache/bbs/service/HospitalService.java
@@ -0,0 +1,17 @@
+package com.mustache.bbs.service;
+
+import com.mustache.bbs.domain.entity.Hospital;
+import com.mustache.bbs.repository.HospitalRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.awt.print.Pageable;
+
+public class HospitalService {
+
+//    @Transactional
+//    public Page<Hospital> getBoardList(Pageable pageable) {
+//        return hospitalRepository.findAll(pageable);
+//
+//    }
+}
