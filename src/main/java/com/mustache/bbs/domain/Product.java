4e17f0ba28807afd155d8c04d3f5e4aabc102266
yejinstar <gyj5586@gmail.com>
2022-11-14 11:20:50 +0900

    product 테이블 생성

---
+++ src/main/java/com/mustache/bbs/domain/Product.java
@@ -0,0 +1,25 @@
+package com.mustache.bbs.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "prodct")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String price;
+
+    @Column(nullable = false)
+    private String stock;
+
+    private LocalDateTime createAt;
+    private LocalDateTime updatedAt;
+
+}
