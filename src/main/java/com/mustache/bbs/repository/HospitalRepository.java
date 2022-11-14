b03bdf2d5756adf22a0da84ead9676c9a1fd2aed
yejinstar <gyj5586@gmail.com>
2022-11-14 15:10:50 +0900

    병상 수가 10개 이상 20개 미만인 병원 찾는 쿼리

--- src/main/java/com/mustache/bbs/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs/repository/HospitalRepository.java
@@ -6,5 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface HospitalRepository extends JpaRepository<Hospital,Integer> {
+
+    List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
+    List<Hospital> findByHospitalNameStartsWith(String keyword); // 시작
+    List<Hospital> findByHospitalNameEndingWith(String keyword); // 끝남
+
+    List<Hospital> findByPatientRoomCountBetween(Integer start, Integer end);
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
     List<Hospital> findByBusinessTypeNameInAndRoadNameAddressLike(List<String> businessTypes, String roadNameAddress);
 }
--- src/test/java/com/mustache/bbs/repository/HospitalRepositoryTest.java
+++ src/test/java/com/mustache/bbs/repository/HospitalRepositoryTest.java
@@ -24,8 +24,8 @@ class HospitalRepositoryTest {
     }
 
     @Test
-    @DisplayName("BusinessTypeName이 보건소 보건지소 보건진료소인 데이터 파싱")
-    void findByBusinessTypeNameIn() {
+    @DisplayName("광진구 + BusinessTypeName이 보건소 보건지소 보건진료소인 데이터 파싱")
+    void findByBusinessTypeNameInAndRoadNameAddressLike() {
         List<String> inClues = new ArrayList<>();
         inClues.add("보건소");
         inClues.add("보건진료소");
@@ -35,5 +35,55 @@ class HospitalRepositoryTest {
              ) {
             System.out.println(hospital.getHospitalName());
         }
+    }
+
+    @Test
+    @DisplayName("BusinessTypeName이 보건소 보건지소 보건진료소인 데이터 파싱")
+    void findByBusinessTypeNameIn() {
+        List<String> inClues = new ArrayList<>();
+        inClues.add("보건소");
+        inClues.add("보건진료소");
+        inClues.add("보건지소");
+        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameIn(inClues);
+        for (var hospital:hospitals
+        ) {
+            System.out.println(hospital.getHospitalName());
+        }
+    }
+    // 출력코드
+    void printHospitalNameAndAddress(List<Hospital> hospitals) {
+        for (var hospital : hospitals) {
+            System.out.printf("%s | %s %f | %d\n", hospital.getHospitalName(), hospital.getRoadNameAddress(), hospital.getTotalAreaSize(),
+                    hospital.getPatientRoomCount());
+        }
+
+        System.out.println(hospitals.size());
+    }
+    @Test
+    @DisplayName("containing")
+    void containing() {
+        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining("송파구");
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    @DisplayName("startsWith")
+    void startsWith() {
+        List<Hospital> hospitals = hospitalRepository.findByHospitalNameStartsWith("연세");
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    @DisplayName("endingWith")
+    void endingWith() {
+        List<Hospital> hospitals = hospitalRepository.findByHospitalNameEndingWith("보건소");
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    @DisplayName("patientRoomCount")
+    void patientRoomCount() {
+        List<Hospital> hospitals = hospitalRepository.findByPatientRoomCountBetween(10,19);
+        printHospitalNameAndAddress(hospitals);
     }
 }
