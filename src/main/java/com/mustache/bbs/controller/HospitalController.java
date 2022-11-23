c0635fcf26d20bffad5c68196b7385269888b70a
yejinstar <gyj5586@gmail.com>
2022-11-23 16:30:15 +0900

    병원정보 검색 기능 추가

--- src/main/java/com/mustache/bbs/controller/HospitalController.java
+++ src/main/java/com/mustache/bbs/controller/HospitalController.java
@@ -2,6 +2,7 @@ package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.entity.Hospital;
 import com.mustache.bbs.repository.HospitalRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -10,10 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
+@Slf4j
 @RequestMapping("/hospitals")
 public class HospitalController {
     private final HospitalRepository hospitalRepository;
@@ -23,10 +26,13 @@ public class HospitalController {
     }
 
     @GetMapping(value = "")
-    public String list(Model model, @PageableDefault(size = 20, sort = "id",direction = Sort.Direction.ASC)
+    public String list(@RequestParam(value = "keyword", required = false) String keyword, Model model, @PageableDefault(size = 20, sort = "id",direction = Sort.Direction.ASC)
     Pageable pageable) {
-        Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
+        log.info("keyword : {} ", keyword);
+        //Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
+        Page<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining(keyword, pageable);
         model.addAttribute("hospitals", hospitals);
+        model.addAttribute("keyword", keyword);
         model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
         model.addAttribute("next", pageable.next().getPageNumber());
         return "hospitals/list";
--- src/main/java/com/mustache/bbs/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs/repository/HospitalRepository.java
@@ -1,12 +1,15 @@
 package com.mustache.bbs.repository;
 
 import com.mustache.bbs.domain.entity.Hospital;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface HospitalRepository extends JpaRepository<Hospital,Integer> {
 
+    Page<Hospital> findByRoadNameAddressContaining(String keyword, Pageable pageable);
     List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
     List<Hospital> findByHospitalNameStartsWith(String keyword); // 시작
     List<Hospital> findByHospitalNameEndingWith(String keyword); // 끝남
